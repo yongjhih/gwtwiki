76e5e278a51998a2425c3200e02f0a670fdb0ca6
biletnikov <biletnikov@bd7875de-0725-0410-b300-dd8817ff3526>
2009-04-19 20:19:42 +0000

    Add "edit" action support

---
+++ info.bliki.wiki/bliki-core/src/test/java/info/bliki/api/query/EditTest.java
@@ -0,0 +1,28 @@
+package info.bliki.api.query;
+
+import info.bliki.api.User;
+import info.bliki.api.Connector;
+import info.bliki.api.UnexpectedAnswerException;
+
+/**
+ * Tests edit query.
+ */
+public class EditTest extends BaseQueryTest {
+
+    public void testSmokeTest() {
+        User user = getAnonymousUser();
+        Connector connector = new Connector();
+        connector.login(user);
+
+        try {
+            // there is no rights to do this action. The error must be received.
+            connector.edit(user, Edit.create().title("Main").text("blabla"));
+            fail("UnexpectedAnswerException must be thrown");
+        } catch (UnexpectedAnswerException e) {
+            // ok
+            assertNotNull(e.getErrorData());
+            System.out.println(e.getErrorData());
+        }
+    }
+
+}
