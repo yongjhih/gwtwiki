e7c49d7e171fdb7bf378077ad5400f33f646e757
axelclk <axelclk@bd7875de-0725-0410-b300-dd8817ff3526>
2009-08-26 19:11:02 +0000

    Multiple lines in comment mode aren't shown correctly

--- info.bliki.wiki/bliki-core/src/test/java/info/bliki/wiki/tags/code/ABAPTest.java
+++ info.bliki.wiki/bliki-core/src/test/java/info/bliki/wiki/tags/code/ABAPTest.java
@@ -4,30 +4,28 @@ import info.bliki.wiki.filter.FilterTestSupport;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-
-public class ABAPTest extends FilterTestSupport
-{
-	public ABAPTest(String name)
-	{
+public class ABAPTest extends FilterTestSupport {
+	public ABAPTest(String name) {
 		super(name);
 	}
 
-	public static Test suite()
-	{
+	public static Test suite() {
 		return new TestSuite(ABAPTest.class);
-	} 
+	}
 
-	public void testABAP()
-	{
+	public void testABAP() {
 		String result = wikiModel.render("'''ABAP Example'''\n" + "<source lang=abap>\n" + "*--- line comment\n"
-				+ "WRITE: / '''Hello World''' \"test comment\n" + "< > \" \' &" + "}\n" + "</source>");
+				+ "*--- line comment\n" + "WRITE: / '''Hello World''' \"test comment\n" + "< > \" \' &" + "}\n" + "</source>");
 
 		assertEquals(
 				"\n" + 
 				"<p><b>ABAP Example</b>\n" + 
 				"</p><pre class=\"code\"><span style=\"color:#3F7F5F; \">\n" + 
 				"*--- line comment\n" + 
-				"</span><b><span style=\"color:#7F0055; font-weight: bold; \">WRITE</span></b>: / <span style=\"color:#2A00FF; \">&#39;&#39;&#39;Hello World&#39;&#39;&#39;</span> <span style=\"color:#3F7F5F; \">&#34;test comment\n" + 
+				"</span><span style=\"color:#3F7F5F; \">\n" + 
+				"*--- line comment\n" + 
+				"</span>\n" + 
+				"<b><span style=\"color:#7F0055; font-weight: bold; \">WRITE</span></b>: / <span style=\"color:#2A00FF; \">&#39;&#39;&#39;Hello World&#39;&#39;&#39;</span> <span style=\"color:#3F7F5F; \">&#34;test comment\n" + 
 				"</span>&#60; &#62; <span style=\"color:#3F7F5F; \">&#34; &#39; &#38;}\n" + 
 				"</span></pre>", result);
 	}
