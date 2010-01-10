9b3adf116fc83ae1f8b585a8f4a803e7aa905e3e
axelclk <axelclk@bd7875de-0725-0410-b300-dd8817ff3526>
2010-01-10 11:43:00 +0000

    Added most of the jamwiki taglib tags

--- info.bliki.gae/src/org/jamwiki/utils/WikiUtil.java
+++ info.bliki.gae/src/org/jamwiki/utils/WikiUtil.java
@@ -22,6 +22,9 @@ import java.io.IOException;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
 import org.jamwiki.DataHandler;
 import org.jamwiki.Environment;
 
@@ -53,6 +56,34 @@ public class WikiUtil {
 	}
 
 	/**
+   * Create a pagination object based on parameters found in the current
+   * request.
+   *
+   * @param request The servlet request object.
+   * @return A Pagination object constructed from parameters found in the
+   *  request object.
+   */
+  public static Pagination buildPagination(HttpServletRequest request) {
+    int num = Environment.getIntValue(Environment.PROP_RECENT_CHANGES_NUM);
+    if (request.getParameter("num") != null) {
+      try {
+        num = Integer.parseInt(request.getParameter("num"));
+      } catch (NumberFormatException e) {
+        // invalid number
+      }
+    }
+    int offset = 0;
+    if (request.getParameter("offset") != null) {
+      try {
+        offset = Integer.parseInt(request.getParameter("offset"));
+      } catch (NumberFormatException e) {
+        // invalid number
+      }
+    }
+    return new Pagination(num, offset);
+  }
+  
+	/**
    * Utility method to retrieve an instance of the current data handler.
    * 
    * @return An instance of the current data handler.
@@ -63,5 +94,85 @@ public class WikiUtil {
     return new GAEDataHandler();
   }
 
+  /**
+   * Retrieve a virtual wiki name from the servlet request.  This method
+   * will retrieve a request parameter matching the PARAMETER_VIRTUAL_WIKI
+   * value, and will decode it appropriately.
+   *
+   * @param request The servlet request object.
+   * @return The decoded virtual wiki name retrieved from the request.
+   */
+  public static String getVirtualWikiFromRequest(HttpServletRequest request) {
+    String virtualWiki = request.getParameter(WikiUtil.PARAMETER_VIRTUAL_WIKI);
+    if (virtualWiki == null) {
+      virtualWiki = (String)request.getAttribute(WikiUtil.PARAMETER_VIRTUAL_WIKI);
+    }
+    if (virtualWiki == null) {
+      return null;
+    }
+    return Utilities.decodeTopicName(virtualWiki, true);
+  }
 
+  /**
+   * Retrieve a virtual wiki name from the request URI.  This method will
+   * retrieve the portion of the URI that immediately follows the servlet
+   * context and decode it appropriately.
+   *
+   * @param request The servlet request object.
+   * @return The decoded virtual wiki name retrieved from the URI.
+   */
+  public static String getVirtualWikiFromURI(HttpServletRequest request) {
+    String uri = retrieveDirectoriesFromURI(request, 0);
+    if (StringUtils.isBlank(uri)) {
+      logger.info("No virtual wiki found in URL: " + request.getRequestURI());
+      return null;
+    }
+    // default the virtual wiki to the URI since the user may have accessed a URL of
+    // the form /context/virtualwiki with no trailing slash
+    String virtualWiki = uri;
+    int slashIndex = uri.indexOf('/');
+    if (slashIndex != -1) {
+      virtualWiki = uri.substring(0, slashIndex);
+    }
+    return Utilities.decodeAndEscapeTopicName(virtualWiki, true);
+  }
+  
+  /**
+   * Utility method for retrieving values from the URI.  This method
+   * will attempt to properly convert the URI encoding, and then offers a way
+   * to return directories after the initial context directory.  For example,
+   * if the URI is "/context/first/second/third" and this method is called
+   * with a skipCount of 1, the return value is "second/third".
+   *
+   * @param request The servlet request object.
+   * @param skipCount The number of directories to skip.
+   * @return A UTF-8 encoded portion of the URL that skips the web application
+   *  context and skipCount directories, or <code>null</code> if the number of
+   *  directories is less than skipCount.
+   */
+  private static String retrieveDirectoriesFromURI(HttpServletRequest request, int skipCount) {
+    String uri = request.getRequestURI().trim();
+    // FIXME - needs testing on other platforms
+    uri = Utilities.convertEncoding(uri, "ISO-8859-1", "UTF-8");
+    String contextPath = request.getContextPath().trim();
+    if (StringUtils.isBlank(uri) || contextPath == null) {
+      return null;
+    }
+    // make sure there are no instances of "//" in the URL
+    uri = uri.replaceAll("(/){2,}", "/");
+    if (uri.length() <= contextPath.length()) {
+      return null;
+    }
+    uri = uri.substring(contextPath.length() + 1);
+    int i = 0;
+    while (i < skipCount) {
+      int slashIndex = uri.indexOf('/');
+      if (slashIndex == -1) {
+        return null;
+      }
+      uri = uri.substring(slashIndex + 1);
+      i++;
+    }
+    return uri;
+  }
 }
