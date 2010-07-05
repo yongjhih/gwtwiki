6cc5c05b3b784f46e24baca8634ea2f5a5c04cf4
axelclk <axelclk@bd7875de-0725-0410-b300-dd8817ff3526>
2010-07-05 18:18:08 +0000

    Test escaping of JavaSyript

--- info.bliki.wiki/bliki-core/src/test/java/info/bliki/wiki/tags/code/SourceTagTest.java
+++ info.bliki.wiki/bliki-core/src/test/java/info/bliki/wiki/tags/code/SourceTagTest.java
@@ -22,10 +22,22 @@ public class SourceTagTest extends FilterTestSupport {
 	public void testUnknownSourceTag002() {
 		String result = wikiModel.render("start <source lang=unknown>first line\n second line\n <html> third line\n</source> end");
 
-		assertEquals("\n" + 
-				"<p>start </p><pre class=\"unknown\">first line\n" + 
-				" second line\n" + 
-				" &#60;html&#62; third line\n" + 
-				"</pre> end", result);
+		assertEquals("\n" + "<p>start </p><pre class=\"unknown\">first line\n" + " second line\n" + " &#60;html&#62; third line\n"
+				+ "</pre> end", result);
+	}
+
+	/**
+	 * Test escaping of JavaSyript
+	 */
+	public void testUnknownSourceTag003() {
+		final String result = wikiModel.render("'''Example'''\n" + "<source lang=unknown>\n"
+				+ "<form><input type=\"button\" onclick=\"alert('Are you sure you want to do this?')\" value=\"Alert\"></form>\n"
+				+ "</source>");
+		String expect = "\n"
+				+ "<p><b>Example</b>\n"
+				+ "</p><pre class=\"unknown\">\n"
+				+ "&#60;form&#62;&#60;input type=&#34;button&#34; onclick=&#34;alert(&#39;Are you sure you want to do this?&#39;)&#34; value=&#34;Alert&#34;&#62;&#60;/form&#62;\n"
+				+ "</pre>";
+		assertEquals("SQL test002", expect, result);
 	}
 }
