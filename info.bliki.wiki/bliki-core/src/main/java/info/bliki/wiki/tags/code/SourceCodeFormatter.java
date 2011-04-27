0216aec34a4646b035896431917b7eca9d2efd5f
axelclk <axelclk@bd7875de-0725-0410-b300-dd8817ff3526>
2011-04-27 19:51:06 +0000

    Fixed issue #65: Source tag fails to render groovy source properly

--- info.bliki.wiki/bliki-core/src/main/java/info/bliki/wiki/tags/code/SourceCodeFormatter.java
+++ info.bliki.wiki/bliki-core/src/main/java/info/bliki/wiki/tags/code/SourceCodeFormatter.java
@@ -2,9 +2,15 @@ package info.bliki.wiki.tags.code;
 
 /**
  * Displays source code with syntax highlighting
- *
+ * 
  */
-public interface SourceCodeFormatter
-{
+public interface SourceCodeFormatter {
+	/**
+	 * Convert the given <code>content</code> string to HTML synatx highlighted
+	 * string.
+	 * 
+	 * @param content
+	 * @return
+	 */
 	public String filter(String content);
 }
