0063a3f6e19019bd687a17f95d7370355f43784a
axelclk <axelclk@bd7875de-0725-0410-b300-dd8817ff3526>
2012-05-14 21:14:56 +0000

    Javadoc

--- info.bliki.wiki/bliki-core/src/main/java/info/bliki/wiki/tags/IgnoreTag.java
+++ info.bliki.wiki/bliki-core/src/main/java/info/bliki/wiki/tags/IgnoreTag.java
@@ -7,8 +7,8 @@ import info.bliki.wiki.tags.util.INoBodyParsingTag;
 import java.io.IOException;
 
 /**
- * Wiki tag which creates no output. Useful for ignoring wiki exxtension tags,
- * which shouldn't be supported. see <a
+ * Wiki tag which renders no HTML output. This tag is useful for ignoring wiki
+ * extension tags, which shouldn't be supported. See <a
  * href="http://code.google.com/p/gwtwiki/issues/detail?id=94">Issue 94</a>
  * 
  */
