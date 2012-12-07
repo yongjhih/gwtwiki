46143581d535427a77cb9a86147713e825af8bf0
nico.kruber.zib@gmail.com <nico.kruber.zib@gmail.com@bd7875de-0725-0410-b300-dd8817ff3526>
2012-12-07 17:56:43 +0000

    ported unit tests of r8231..r8235 to 3.1

--- info.bliki.wiki/bliki-core/src/test/java/info/bliki/wiki/namespaces/NamespaceTest.java
+++ info.bliki.wiki/bliki-core/src/test/java/info/bliki/wiki/namespaces/NamespaceTest.java
@@ -1,6 +1,9 @@
 package info.bliki.wiki.namespaces;
 
 import info.bliki.wiki.namespaces.Namespace.NamespaceValue;
+
+import java.util.Locale;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -11,13 +14,20 @@ import junit.framework.TestSuite;
  * @author Nico Kruber, kruber@zib.de
  */
 public class NamespaceTest extends TestCase {
-
+	protected Namespace namespace = null;
+	
 	public NamespaceTest(String name) {
 		super(name);
 	}
 
 	public static Test suite() {
 		return new TestSuite(NamespaceTest.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		namespace = new Namespace(Locale.ENGLISH);
 	}
 
 	/**
@@ -31,4 +41,30 @@ public class NamespaceTest extends TestCase {
 		}
 	}
 
+	public void testNamespace001() {
+		assertEquals(namespace.META, namespace.getNamespace("Meta"));
+		assertEquals(namespace.META_TALK, namespace.getNamespace("Meta_talk"));
+		assertEquals(namespace.META_TALK, namespace.getNamespace("Meta talk"));
+		assertEquals(namespace.META, namespace.getNamespace("Project"));
+		assertEquals(namespace.META_TALK, namespace.getNamespace("Project_talk"));
+		assertEquals(namespace.META_TALK, namespace.getNamespace("Project talk"));
+	}
+
+	public void testTalkspace001() {
+		assertEquals(namespace.META_TALK, namespace.getTalkspace("Meta"));
+		assertEquals(namespace.META_TALK, namespace.getTalkspace("Meta_talk"));
+		assertEquals(namespace.META_TALK, namespace.getTalkspace("Meta talk"));
+		assertEquals(namespace.META_TALK, namespace.getTalkspace("Project"));
+		assertEquals(namespace.META_TALK, namespace.getTalkspace("Project_talk"));
+		assertEquals(namespace.META_TALK, namespace.getTalkspace("Project talk"));
+	}
+
+	public void testContentspace001() {
+		assertEquals(namespace.META, namespace.getContentspace("Meta"));
+		assertEquals(namespace.META, namespace.getContentspace("Meta_talk"));
+		assertEquals(namespace.META, namespace.getContentspace("Meta talk"));
+		assertEquals(namespace.META, namespace.getContentspace("Project"));
+		assertEquals(namespace.META, namespace.getContentspace("Project_talk"));
+		assertEquals(namespace.META, namespace.getContentspace("Project talk"));
+	}
 }
